f9e44bbe871ac874beeaa48e672d16efc52e52a4
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-06-04 21:44:09 +0900

    feat : 13주차 A-01

--- week13/02student/student-crud-stu-01.js
+++ week13/02student/student-crud-stu-01.js
@@ -1,7 +1,6 @@
 // ./02student/student-crud.js
-const MongoClient = require("mongodb").MongoClient;
-// const uri = '    ?retryWrites=true&w=majority';
-
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://admin:1234@cluster0.t3dchvm.mongodb.net/testDB?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
 
 async function run() {
@@ -9,21 +8,39 @@ async function run() {
         await client.connect();
         console.log('DB Connected');
 
-        // 컬렉션
-
+        const database = client.db('testDB');
+        const collection = database.collection('students');
+        
         // 추가
+        const newUser = { name: 'huiwoo-jo', age: 23, email: 'huiwoo-jo@gmail.com'};
+        const insertResult = await collection.insertOne(newUser);
+        users = await collection.find({}).toArray();
+        console.log('입력 완료\n문서 검색: ', users);
+        
 
         // 검색
+        users = await collection.find({}).toArray();
+        console.log('문서 검색: ', users);
+
 
         // 수정
+        const updatedUser = await collection.findOneAndUpdate(
+            { name: 'huiwoo-jo' },
+            { $set: { age: 23 } },
+            { email: 'huiwoo-jo@dongyang.ac.kr' }
+        );
+        users = await collection.find({}).toArray();
+        console.log('수정 완료\n수정된 문서:  ', updatedUser);
 
         // 삭제
+        const deleteResult = await collection.deleteMany({ name: "huiwoo-jo" });
+        console.log('삭제 완료');
         
-        // 연결 종료
+
         await client.close();
-    } catch (err) {
+    } catch(err){
       console.error(err);
     }
-  };
-  
-  run();
+};
+
+run();
