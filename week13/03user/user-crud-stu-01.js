b50658cbca991c5061f4bbeb386dd57732f01ac6
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-06-04 20:42:17 +0900

    build : 13주차 수업 중 과제

--- week13/03user/user-crud-stu-01.js
+++ week13/03user/user-crud-stu-01.js
@@ -1,22 +1,15 @@
 // ./03person/user-crud.js
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
-
-
+const Usesr = require("./models/userModel");
 mongoose.set("strictQuery", false);
-
 const app = express();
 app.use(bodyParser.json());
 
 app.listen(3000, async () => {
     console.log("3000번 포트에서 서버 실행중");
-//    const mongodbUri = '             /testDB?retryWrites=true&w=majority';
-
-    // mongoose.connect(mongodbUri, { useNewUrlParser: true })
-
-    
+    const mongodbUri = 'mongodb+srv://admin:1234@cluster0.t3dchvm.mongodb.net/testDB?retryWrites=true&w=majority';
+    mongoose.connect(mongodbUri).then(console.log('DB Connected'));
 });
 
 // user 전체 정보 검색
@@ -27,13 +20,17 @@ app.get('/user', async (req, res) => {
 });
 
 // 특정 name 으로 검색
-app.get('', async (req, res) => {
-
+app.get('/uer/:name', async (req, res) => {
+    const user = await User.findOne(
+        {name:req.params.name}
+    );
+    console.log(user);
+    res.send(user);
 });
 
 // 추가
 app.post('', async (req, res) => {
-
+    
 });
 
 // 수정
